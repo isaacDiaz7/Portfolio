e341cabc56d71a21b53c250cb73b49fbc2c4051b
isaacDiaz7 <isaac.n.diaz7@gmail.com>
2024-01-22 20:46:34 -0800

    Fixed minor bugs

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -42,10 +42,10 @@ const EmailSection = () => {
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md "> I am currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I will try my best to get back to you!</p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com/isaacDiaz7">
+          <Link href="http://github.com/isaacDiaz7">
             <Image src={GithubIcon} alt="Github Icon" width={30} height={30} />
           </Link>
-          <Link href="linkedin.com/in/isaacndiaz">
+          <Link href="http://linkedin.com/in/isaacndiaz">
             <Image src={LinkedinIcon} alt="Linkedin Icon" width={30} height={30} />
           </Link>
         </div>
