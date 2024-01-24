e47200052c5fcb6734bc88fcc2962756d1796bc9
isaacDiaz7 <isaac.n.diaz7@gmail.com>
2024-01-24 08:21:37 -0800

    Fixed slight bug in about me section

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -19,7 +19,8 @@ export async function POST(req, res) {
         <>
           <h1>{subject}</h1>
           <p>Thank you for contacting me!</p>
-          <p>New message submitted:</p>
+          <p>I will get back to you as soon as possible!</p>
+          <p>Your message that was submitted:</p>
           <p>{message}</p>
         </>
       ),
--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
         <Image src="/images/about-image.jpeg" alt="Desktop computer" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">I am a full stack software engineer with a passion for creating interactive and responsive web application. I have experience working with JavaScript, React, Node.js, Git, SQL, .NET, POSTMAN, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.</p>
+          <p className="text-base lg:text-lg">I am a full stack software engineer with a passion for creating interactive and responsive web application. I have experience working with JavaScript, C#, React, Node.js, SQL, .NET, POSTMAN, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.</p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
               {" "}
